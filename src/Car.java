8031896f485699acbb47043157e9af36613f2b42
Vally <murkhonovbatoha@gmail.com>
2023-03-18 08:06:04 +0800

    body

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle implements TransportTyre, TransportEngine {
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bicycle[] bicycles = {
+                new Bicycle("Моноцикл", 1),
+                new Bicycle("Велосипед", 2)
+        };
+        Car[] cars = {
+                new Car("Трицикл", 3),
+                new Car("Машина", 4)
+        };
+        Truck[] trucks = {
+                new Truck("Внедорожник", 4),
+                new Truck("Трэкол", 6)
+        };
+
+        ServiceStation station = new ServiceStation();
+        for (Bicycle bicycle : bicycles) {
+            station.check(bicycle);
+        }
+        for (Car car : cars) {
+            station.check(car);
+        }
+        for (Truck truck : trucks) {
+            station.check(truck);
+        }
     }
 }
+
+
+
